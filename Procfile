ad343729cf0fa7617257efd71e843499c05c4076
RyanTheRyan <ryandl2003@gmail.com>
2020-09-24 20:21:10 -0600

    take two

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn --bind 0.0.0.0:$PORT main-api:app --preload
+web: gunicorn application:app
--- app/__init__.py
+++ app/__init__.py
@@ -17,6 +17,3 @@ def create_app():
     app.register_blueprint(main)
 
     return app
-
-if __name__ == "__main__":
-    app.run()
